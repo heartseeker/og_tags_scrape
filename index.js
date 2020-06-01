1cfa046ec2e09f69803adb929918eda834894aa7
Alex Amistad <amal@erni.ph>
2020-06-01 09:48:44 +0800

    update files

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
 const url = require('url');
-var fs = require('fs');
-var https = require('https');
 let grabity = require("grabity");
 var port = process.env.PORT || 3000; 
-const cors = require('cors')
+const cors = require('cors');
 
 app.use(cors())
 
@@ -14,27 +12,11 @@ app.get('/scrape',  async (req, res) => {
   const url_parts = url.parse(req.url, true);
   const query = url_parts.query.url;
 
-  console.log(query);
   const it = await grabity.grabIt(query);
 
   res.json(it);
  });
 
-app.listen(3000, () => {
+app.listen(port, () => {
  console.log('Server running on port 3000');
 });
-
-
-// https.createServer({
-//   key: fs.readFileSync('server.key'),
-//   cert: fs.readFileSync('server.cert')
-// }, app)
-// .listen(port, function () {
-// console.log(`Go to https://localhost:${port}/`)
-// })
-
-// process.on('uncaughtException', function(e) {
-//   console.log('An error has occured. error is: %s and stack trace is: %s', e, e.stack);
-//   console.log("Process will restart now.");
-//   process.exit(1);
-// })
